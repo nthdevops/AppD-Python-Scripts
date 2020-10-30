ada7093c8919ac20434265e87a91e3c19e2b4dfd
nthdevops <nthmanager@gmail.com>
2020-10-30 11:45:54 -0300

    Using Flask

--- zoomMeetingCreating/meeting.py
+++ zoomMeetingCreating/meeting.py
@@ -1,6 +1,9 @@
 import http.client as httpc
-from time import time
 import datetime, sys, jwt, json
+from time import time
+from flask import Flask, request
+
+app = Flask(__name__)
 
 # create a function to generate a token using the pyjwt library
 def generateToken():
@@ -17,46 +20,53 @@ def mkRequest(method,url,bodyIn,headersIn):
 	data = res.read()
 	return data.decode('utf-8')
 
-#Emails
-emails = []
-#Adding emails from args
-emails.append(sys.argv[1])
-emails.append(sys.argv[2])
+@app.route('/', methods=['POST'])
+def createMeeting():
+  content = request.json
+  #Emails
+  emails = []
+  #Adding emails from request
+  emails.append(content["result"]["emailatendente"])
+  emails.append(content["result"]["cliente"])
 
-#Names
-names = []
-for email in emails:
-  names.append(email.split("@")[0])
+  #Names
+  names = []
+  for email in emails:
+    names.append(email.split("@")[0])
 
-#Meeting creating config
-meetingName = "Virtual Techbar Supporting "+emails[1]
-date = datetime.datetime.now()
-jsonBodyCreate = {
-  "topic": meetingName,
-  "type": 2,
-  "start_time": "{:%Y-%m-%dT%H:%M:%SZ}".format(date),
-  "duration": 60,
-  "timezone": "America/Sao_Paulo",
-  "agenda": meetingName,
-  "settings": {
-    "approval_type": 0,
-    "registrants_email_notification": "true",
-    "meeting_authentication" : "false",
-    "use_pmi": "false"
+  #Meeting creating config
+  meetingName = "Virtual Techbar Supporting "+emails[1]
+  date = datetime.datetime.now()
+  jsonBodyCreate = {
+    "topic": meetingName,
+    "type": 2,
+    "start_time": "{:%Y-%m-%dT%H:%M:%SZ}".format(date),
+    "duration": 60,
+    "timezone": "America/Sao_Paulo",
+    "agenda": meetingName,
+    "settings": {
+      "approval_type": 0,
+      "registrants_email_notification": "true",
+      "meeting_authentication" : "false",
+      "use_pmi": "false"
+    }
   }
-}
-urlCreate = "https://api.zoom.us/v2/users/L0rjFIgoSDWjZ74S4U9bag/meetings"
+  urlCreate = "https://api.zoom.us/v2/users/L0rjFIgoSDWjZ74S4U9bag/meetings"
 
-#Meeting creation
-retCreation = json.loads(mkRequest("POST", urlCreate, json.dumps(jsonBodyCreate), headers))
+  #Meeting creation
+  retCreation = json.loads(mkRequest("POST", urlCreate, json.dumps(jsonBodyCreate), headers))
 
-#Inviting Users
-urlInvite = "https://api.zoom.us/v2/meetings/MEETINGID/registrants".replace("MEETINGID", str(retCreation["id"]))
+  #Inviting Users
+  urlInvite = "https://api.zoom.us/v2/meetings/MEETINGID/registrants".replace("MEETINGID", str(retCreation["id"]))
 
-for i in range(2):
-  jsonBodyInvite = {"email": "", "first_name": ""}
-  email = emails[i]
-  name = names[i]
-  jsonBodyInvite["email"] = email
-  jsonBodyInvite["first_name"] = name
-  mkRequest("POST", urlInvite, json.dumps(jsonBodyInvite), headers)
+  for i in range(2):
+    jsonBodyInvite = {"email": "", "first_name": ""}
+    email = emails[i]
+    name = names[i]
+    jsonBodyInvite["email"] = email
+    jsonBodyInvite["first_name"] = name
+    mkRequest("POST", urlInvite, json.dumps(jsonBodyInvite), headers)
+  
+  return {'result':'Ok'}
+
+app.run(host='0.0.0.0', port=9090)
